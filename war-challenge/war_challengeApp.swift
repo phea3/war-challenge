24298ae429b76c2c294a74806a89413f3d2e064b
Mouygech Leang <leangmouygech68@gmail.com>
2022-05-16 14:23:17 +0700

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Leng Mouyngech on 16/5/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
